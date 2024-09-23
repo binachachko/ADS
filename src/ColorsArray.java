952976eac17d1bc2ffcd100e1c1a3ca27ff0468f
Bina <binachachko@gmail.com>
2024-09-23 18:33:34 +0300

    Task Java ArrayList - exercise is done

---
+++ src/ColorsArray.java
@@ -0,0 +1,13 @@
+public class colorarray {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Purple");
+        System.out.println("Hello World");
+    }
+}
